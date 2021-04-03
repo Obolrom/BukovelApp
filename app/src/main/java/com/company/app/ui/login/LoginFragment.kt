309c8796376dbf5e363d52c5dcd014a39f8491d0
Obolrom <obolrom@gmail.com>
2021-04-03 21:24:05 +0300

    feat: login frag

---
+++ app/src/main/java/com/company/app/ui/login/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.company.app.ui.login
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.company.app.R
+
+class LoginFragment : Fragment() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        loginViewModel =
+                ViewModelProvider(this).get(LoginViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_login, container, false)
+
+
+        return root
+    }
+}
