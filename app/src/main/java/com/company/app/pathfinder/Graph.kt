5cb5961e5d75e4643dab9122dd3c7abdb9a3f1b9
Obolrom <obolrom@gmail.com>
2021-05-07 21:53:54 +0300

    feat: edges of graph

--- app/src/main/java/com/company/app/pathfinder/Graph.kt
+++ app/src/main/java/com/company/app/pathfinder/Graph.kt
@@ -1,23 +1,21 @@
 package com.company.app.pathfinder
 
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 class Graph(vertices: Int) {
-    private var adjacencyList: MutableMap<Int, MutableList<Edge>> =
+    private var adjacencyMap: MutableMap<Int, MutableList<Edge>> =
             Hashtable(vertices * 2)
     val vertexAmount: Int
-        get() = adjacencyList.size
+        get() = adjacencyMap.size
 
     init {
         for (i in 0 until vertices)
-            adjacencyList[i] = LinkedList()
+            adjacencyMap[i] = LinkedList()
     }
 
     fun removeSlope(name: String) {
         val forDeletion = mutableListOf<Edge>()
-        for (slope in adjacencyList) {
+        for (slope in adjacencyMap) {
             for (edge in slope.value) {
                 if (edge.pieceOf == name)
                     forDeletion.add(edge)
@@ -28,11 +26,11 @@ class Graph(vertices: Int) {
     }
 
     fun getAdjacentEdges(vertex: Edge): List<Edge> {
-        return adjacencyList[vertex.destination] ?: listOf()
+        return adjacencyMap[vertex.destination] ?: listOf()
     }
 
     fun addEdge(edge: Edge) {
-        val node = adjacencyList[edge.start]
+        val node = adjacencyMap[edge.start]
         node?.add(edge)
     }
 }
