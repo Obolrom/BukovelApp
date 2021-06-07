a48bc1b358e469062d20536f050b69878b1dee27
Obolrom <obolrom@gmail.com>
2021-06-07 21:55:19 +0300

    refac: lift adapter

--- app/src/main/java/com/company/app/ui/lifts/LiftsAdapter.kt
+++ app/src/main/java/com/company/app/ui/lifts/LiftsAdapter.kt
@@ -7,45 +7,43 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.company.app.R
 import com.company.app.ui.map.Lift
+import com.company.app.ui.misc.Adapter
 import java.util.*
 
 class LiftsAdapter(private val context: Context,
-    private val callback: OnLiftClickListener) :
-        ListAdapter<Lift, LiftsAdapter.ViewHolder>(LiftComparator()) {
+    private val callback: OnLiftClickListener
+) : Adapter<Lift>(LiftComparator(), R.layout.item_lift) {
     interface OnLiftClickListener {
         fun onLiftClick(lift: Lift)
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class LiftViewHolder(itemView: View) :
+        Adapter.BaseViewHolder<Lift>(itemView) {
         private val textView: AppCompatTextView = itemView.findViewById(R.id.item_lift_tv)
 
-        fun bind(lift: Lift) {
-            textView.text = lift.name.toUpperCase(Locale.ROOT)
+        override fun bind(item: Lift) {
+            textView.text = item.name.uppercase(Locale.ROOT)
             // FIXME: 20.05.21 loading
 //            textView.background = lift.
-            if ( ! lift.active)
+            if ( ! item.active)
                 textView.setBackgroundColor(ContextCompat
                     .getColor(context.applicationContext, R.color.light_grey))
             itemView.setOnClickListener {
-                callback.onLiftClick(lift)
+                callback.onLiftClick(item)
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BaseViewHolder<Lift> {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.item_lift, parent, false)
 
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val itemView = getItem(position)
-        holder.bind(itemView)
+        return LiftViewHolder(view)
     }
 
     class LiftComparator : DiffUtil.ItemCallback<Lift>() {
